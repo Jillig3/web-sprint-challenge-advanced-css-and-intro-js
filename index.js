943c906aabfc912e8c07680c14207122ba2c78e6
unknown <jacobillig@gmail.com>
2021-08-13 12:26:37 -0500

    Task 6 complete

--- index.js
+++ index.js
@@ -282,10 +282,11 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array, id, name, years, genre, nationality, bio) {
+  array.push({id, name, years, genre, nationality, bio});
+  return array;
 }
-
+console.log(addArtist(artists, 20, 'Jacob Illig', '1992 - current', 'web design', 'United States of America', 'lorem ipsum'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
